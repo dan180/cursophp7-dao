ae3495aca139820c677c146600599fc95b758596
dan180 <danielsantos180@gmail.com>
2018-11-15 17:59:42 -0200

    metodo listas

--- index.php
+++ index.php
@@ -2,11 +2,26 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-$root->loadById(3);
 
-echo $root;
+//carrega um usuario
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
+
+//carrega lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//busca uma lista de usuario pelo login
+//$search = Usuario::search("a");
+//echo json_encode($search);
+
+//carrega usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Daniel","123");
+
+echo $usuario;
 
 
 //$sql = new Sql();
